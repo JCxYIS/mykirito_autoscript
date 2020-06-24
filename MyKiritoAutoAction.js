96e8acb3e5a6a321a89084ac0eadeaf5f2095f5e
JCxYIS <jsn811@gmail.com>
2020-06-24 10:51:53 +0800

    更新，可以用就好

--- MyKiritoAutoAction.js
+++ MyKiritoAutoAction.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         MyKiritoScript(TamperMonkey)
+// @name         MyKirito Auto Action
 // @namespace    https://jcjc.cf/
-// @version      1.1
-// @description   My First TM script!!
+// @version      1.3
+// @description  我的第一支油猴腳本，所以很破對不起。
 // @author       JCxYIS
-// @match        https://mykirito.com/
+// @match        https://mykirito.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -14,18 +14,30 @@
 
     // 參數
     const ACTION_NAME_LIST = ["狩獵兔肉", "自主訓練", "外出野餐", "汁妹", "做善事", "坐下休息", "釣魚"];
-    const BUTTON_AVAILABLE_CLASSNAME = "sc-AxgMl sc-fznZeY bbwYrD";
+    const BUTTON_AVAILABLE_CLASSNAME = "sc-AxgMl llLWDd";
+    const BUTTON_DISABLED_CLASSNAME = "sc-AxgMl kPlkaT";
+    const H3_CLASSNAME = "sc-fznyAO CWQMf";
     const CONTAINER_CLASSNAME = "sc-fzokOt hLgJkJ";
+    
+    const INJECTION_CLASSNAME = "sc-fzplWN hRBsWH"
 
     // 製作選單
     let inject = document.createElement('div');
-    inject.className = "sc-fzplWN hRBsWH";
     let title = document.createElement("h3");
-    title.innerHTML = "自律行動";
+    let subtitle1 = document.createElement('p');
+    let subtitle2 = document.createElement('p');
+    document.getElementsByClassName(CONTAINER_CLASSNAME)[0].insertBefore(inject, 
+        document.getElementsByClassName(CONTAINER_CLASSNAME)[0].childNodes[2])//.appendChild(inject);
     inject.appendChild(title);
+    inject.appendChild(subtitle1);
+    inject.appendChild(subtitle2);
+    inject.classList = INJECTION_CLASSNAME;
+    title.classList = H3_CLASSNAME;
+    title.innerHTML = "自律行動";
+    subtitle1.innerHTML = "自動點按行動按鍵，當按鈕能被按下時就會按下。<span style='color:yellow'>注意：這就是團長說的腳本了，出事請自行負責</span>";
 
     // 獲取可點的按鈕
-    var allbuttons = document.getElementsByTagName('button');
+    let allbuttons = document.getElementsByTagName('button');
     var actionButtons = [];
     for(let i = 0; i < allbuttons.length; i++)
     {
@@ -50,17 +62,16 @@
         newButt.onclick = ()=>{ StartRecursive(actionButtons[i]); }
         inject.appendChild(newButt)
     }
-
-    // write
     let newButt = document.createElement('button');
-    newButt.innerHTML = "停止遞迴";
+    newButt.innerHTML = "停止";
+    newButt.classList = BUTTON_AVAILABLE_CLASSNAME;
     newButt.onclick = ()=>{ EndRecursive(); }
-    inject.appendChild(newButt)
-    document.getElementsByClassName(CONTAINER_CLASSNAME)[0].appendChild(inject);
+    inject.appendChild(newButt);
+    
 
 
 
-    // fuc
+    // 以下是功能
     var recursiveTimerId;
     var clickCombo = 0;
     var continuousClick = 0;
@@ -69,6 +80,11 @@
     function StartRecursive(butt)
     {
         console.log("開始遞迴：按鈕"+butt.innerHTML);
+        for(var i = 0; i < actionButtons.length; i++)
+        {
+            actionButtons[i].disabled = true;
+        }
+        EndRecursive();
         Recursive(butt);
     }
     function Recursive(butt)
@@ -77,7 +93,7 @@
         let nextTime = 5000+Math.random()*15000;
 
 
-        if(butt.disabled)
+        if(butt.disabled || butt.classList == BUTTON_DISABLED_CLASSNAME)
         {
             // 仍在鎖，不能按
             isLastActionClick = false;
@@ -97,7 +113,8 @@
             isLastActionClick = true;
 
             console.log(clickCombo+" combo!! "+butt.innerHTML + "\n下次檢查時間(n秒後)："+nextTime/1000);
-        }
+            console.log(butt);
+        }        
 
         if(continuousClick < 5)
         {
@@ -106,14 +123,24 @@
         else
         {
             console.warn("無效點擊達 5 次。請重新執行遞迴！");
+            EndRecursive();
         }
+
+        subtitle2.innerHTML = "自律行動執行中，已點擊了 "+clickCombo+" 次！";
+        if(continuousClick > 0)
+            subtitle2.innerHTML += "<br><span style='color:yellow'>連續點擊次數( 5 次將會暫停腳本)："+continuousClick+" / 5";
     }
     function EndRecursive()
     {
         window.clearTimeout(recursiveTimerId);
         continuousClick = 0;
         isLastActionClick = false;
-        console.log("停止遞迴；清除參數");
+        for(var i = 0; i < actionButtons.length; i++)
+        {
+            actionButtons[i].disabled = false;
+        }
+        subtitle2.innerHTML = "";
+        console.log("停止遞迴；清除參數。");
     }
 }
 )();
